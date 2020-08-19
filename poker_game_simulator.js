7001c0d4a5ad8da365c16ccb01640df58acd7daa
olli-hash <52583865+olli-hash@users.noreply.github.com>
2020-08-19 18:01:13 +0200

    l

---
+++ poker_game_simulator.js
@@ -0,0 +1,26 @@
+
+/*
+1. create players
+2. create table and set players & stacks
+2a. create game events list
+3. give cards (create cards) and start interviewing
+*/
+
+
+
+/*
+interview-chain:
+
+1,2,3,4,5,6
+1,2,4 --> 1,2,3
+1,2,3
+1,3 --> 1,2
+1,2
+2 --> Winner (originally: 3)
+
+
+
+
+
+
+*/
