d82384544e110b65f42175ac756a73df1fc6f151
olli-hash <52583865+olli-hash@users.noreply.github.com>
2020-09-06 17:41:29 +0200

    m

---
+++ ui_effects.js
@@ -0,0 +1,70 @@
+
+$(document).ready(function(){
+
+    $('#button1').on("click", function(){
+
+        //alert($(this).prev().val())
+
+        $('form.form_of_questions_and_statements').css("margin", "0px 0px 0px 0px;")
+        $('form.form_of_questions_and_statements').css("float", "left")
+
+        l(
+            $('form.form_of_questions_and_statements').css("float")
+        )
+    })
+
+    $('#button2').on("click", function(){
+
+        $('form.form_of_questions_and_statements').css("float", "right")
+
+        l(
+            $('form.form_of_questions_and_statements').css("float")
+        )
+    })
+
+    $('#button2_A').on("click", function(){
+
+        $('form.form_of_questions_and_statements').css("float", "none")
+
+        l(
+            $('form.form_of_questions_and_statements').css("float")
+        )
+    })
+
+    $('#button3').on("click", function(){
+
+        //alert($(this).prev().val())
+
+        $('form.form_of_questions_and_statements').css("margin-top", "0px")
+
+        l(
+            $('form.form_of_questions_and_statements').css("margin-top")
+
+        )
+    })
+
+    $('#button4').on("click", function(){
+
+        //alert($(this).prev().val())
+
+        $('form.form_of_questions_and_statements').css("margin-top", "-111px")
+
+        l(
+            $('form.form_of_questions_and_statements').css("margin-top")
+
+        )
+
+    })
+
+
+})
+
+
+
+
+$(document).ready(function(){
+
+    $('#control_width').on("click", function(){
+        $('form.form_of_questions_and_statements').css("width", $('#control_width').val() )
+    })
+})
