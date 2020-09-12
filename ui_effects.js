ad81d2cb4a35f983073e6d536bc8c3d8102bb160
olli-hash <52583865+olli-hash@users.noreply.github.com>
2020-09-12 22:31:33 +0200

    m

--- ui_effects.js
+++ ui_effects.js
@@ -6,83 +6,80 @@ $(document).ready(function(){
     // $('.dimensions').append(buttons_dimension)
 
 
+    $('a').each(function(index, element){
+        var attr = $(element).attr("href")
+        var html = $(element).html()
+        var sub = html.substring(8)
+        $(element).html(sub)
+
+    })
+
+    $('#position_switch').on("click", function() {
+        var target = $('#target_switch').val()
+        var value = $(this).val()
+        $(target).css("position", value)
+    })
+
     $('#button_shadow_off').on("click", function(){
-        $('.short_container, .chart-container, form').css("box-shadow", "none")
+        var target = $('#target_switch').val()
+        $(target).css("box-shadow", "none")
 
     })
 
     $('#button_shadow_on').on("click", function(){
-        $('.short_container, .chart-container, form').css("box-shadow", "0px 0px 5px 5px black")
+        var target = $('#target_switch').val()
+        $(target).css("box-shadow", "0px 0px 5px 5px black")
 
     })
 
 
     $('#button1').on("click", function(){
-
-        //alert($(this).prev().val())
-
-        $('form.form_of_questions_and_statements').css("margin", "0px 0px 0px 0px;")
-        $('form.form_of_questions_and_statements').css("float", "left")
-
+        // $('form.form_of_questions_and_statements').css("margin", "0px 0px 0px 0px;")
+        var target = $('#target_switch').val()
+        $(target).css("float", "left")
         l(
-            $('form.form_of_questions_and_statements').css("float")
+            $(target).css("float")
         )
     })
 
     $('#button2').on("click", function(){
-
-        $('form.form_of_questions_and_statements').css("float", "right")
-
+        var target = $('#target_switch').val()
+        $(target).css("float", "right")
         l(
-            $('form.form_of_questions_and_statements').css("float")
+            $(target).css("float")
         )
     })
 
     $('#button2_A').on("click", function(){
-
-        $('form.form_of_questions_and_statements').css("float", "none")
-
+        var target = $('#target_switch').val()
+        $(target).css("float", "none")
         l(
-            $('form.form_of_questions_and_statements').css("float")
+            $(target).css("float")
         )
     })
 
     $('#button3').on("click", function(){
-
-        //alert($(this).prev().val())
-
-        $('form.form_of_questions_and_statements').css("margin-top", "0px")
-
+        var target = $('#target_switch').val()
+        $(target).css("margin-top", "0px")
         l(
-            $('form.form_of_questions_and_statements').css("margin-top")
-
+            $(target).css("margin-top")
         )
     })
 
     $('#button4').on("click", function(){
-
-        //alert($(this).prev().val())
-
-        $('form.form_of_questions_and_statements').css("margin-top", "-111px")
-
+        var target = $('#target_switch').val()
+        $(target).css("margin-top", "-111px")
         l(
-            $('form.form_of_questions_and_statements').css("margin-top")
-
+            $(target).css("margin-top")
         )
-
     })
 
     $('#button5').on("click", function(){
-
-        //alert($(this).prev().val())
-
-        $('form.form_of_questions_and_statements').css("margin-top", "50px")
-
+        var target = $('#target_switch').val()
+        $(target).css("margin-top", "50px")
         l(
-            $('form.form_of_questions_and_statements').css("margin-top")
-
+            $(target).css("margin-top")
         )
-
     })
 
 
@@ -92,8 +89,8 @@ $(document).ready(function(){
 
 
 $(document).ready(function(){
-
     $('#control_width').on("click", function(){
-        $('form.form_of_questions_and_statements').css("width", $('#control_width').val() + "%")
+        var target = $('#target_switch').val()
+        $(target).css("width", $('#control_width').val() + "%")
     })
 })
